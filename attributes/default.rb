a73097b15bd58ec1360792656dfe1f1b72bf90d9
Scott Richardson <dev@grogwood.com>
2020-09-06 13:16:05 -0700

    Reference to port data bag item updated

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,7 +9,7 @@ default[:olyn_varnish][:host] = '0.0.0.0'
 default[:olyn_varnish][:frontend_ports_data_bag_item] = 'varnish'
 
 # Backend info
-default[:olyn_varnish][:backend_ports_data_bag_item] = 'www'
+default[:olyn_varnish][:backend_ports_data_bag_item] = 'www_http'
 
 # Varnish version
 default[:olyn_varnish][:major_version] = 6.1
