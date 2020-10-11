cc8787daf7a56635505c35b0f17db0df7c63eea6
Scott Richardson <dev@grogwood.com>
2020-10-10 20:02:31 -0700

    Complete cookbook cleanup, Varnish 6.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,14 @@
-# Storage
-default[:olyn_varnish][:storage_method] = 'malloc'
-default[:olyn_varnish][:malloc_percent] = 33
+# Varnish version to install
+default[:olyn_varnish][:repo][:major_version] = 6.1
 
-# Default host
-default[:olyn_varnish][:host] = '0.0.0.0'
+# Varnish daemon configs
+default[:olyn_varnish][:daemon][:config][:storage_method] = 'malloc'
+default[:olyn_varnish][:daemon][:config][:malloc_percent] = 33
+default[:olyn_varnish][:daemon][:config][:default_ttl] = 3600
+default[:olyn_varnish][:daemon][:config][:host] = '0.0.0.0'
 
 # Frontend listener info
-default[:olyn_varnish][:frontend_ports_data_bag_item] = 'varnish'
+default[:olyn_varnish][:frontend][:ports_data_bag_item] = 'varnish'
 
-# Backend info
-default[:olyn_varnish][:backend_ports_data_bag_item] = 'www_http'
-
-# Varnish version
-default[:olyn_varnish][:major_version] = 6.1
+# Backend listener info
+default[:olyn_varnish][:backend][:ports_data_bag_item] = 'www_backend_http'
